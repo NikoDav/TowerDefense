66536d2864a9a6cffd6292feb5579bf9dbcdf327
NikoDav <165057786+NikoDav@users.noreply.github.com>
2025-02-23 11:56:15 -0500

    add day night cycle

---
+++ Assets/Scripts/DayNightCycle.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DayNightCycle : MonoBehaviour
+{
+    [SerializeField] private float _currentTime;
+    [SerializeField] private float _dayTime;
+    [SerializeField] private DayNightStates _state;
+    [SerializeField] private Transform _light;
+
+    public event UnityAction _daySwitched;
+    public event UnityAction _nightSwitched;
+
+    //day: [34, -22, 10]
+
+    //night: [-13, -19, 28]
+
+    private void Update()
+    {
+        if(_state == DayNightStates.Day)
+        {
+            _currentTime += Time.deltaTime;
+        }
+        if(_currentTime >= _dayTime)
+        {
+            SwitchNight();
+        }
+    }
+
+    public void SwitchDay()
+    {
+        _state = DayNightStates.Day;
+        _daySwitched?.Invoke();
+    }
+
+    public void SwitchNight()
+    {
+        _state = DayNightStates.Night;
+        _nightSwitched?.Invoke();
+    }
+
+    
+}
---
+++ Assets/Scripts/DayNightStates.cs
@@ -0,0 +1,5 @@
+public enum DayNightStates
+{
+    Day,
+    Night
+}
