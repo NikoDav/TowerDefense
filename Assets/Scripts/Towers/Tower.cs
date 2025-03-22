da46daa1a3617ceb080cc33c99d7361ae49cf347
NikoDav <165057786+NikoDav@users.noreply.github.com>
2025-03-22 13:56:10 -0400

    add change mesh

--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -7,11 +7,23 @@ public class Tower : MonoBehaviour
     [SerializeField] private TowerConfig _towerConfig;
     [SerializeField] private MeshFilter _meshFilter;
     private int _index;
+    private Coroutine _meshSwitch;
+    const int _delay = 3;
 
     [ContextMenu("LevelUp")]
     public void LevelUp()
     {
+        if (_meshSwitch != null)
+            return;
         _index++;
+        _meshSwitch = StartCoroutine(MeshSwitch());
+    }
+
+    private IEnumerator MeshSwitch()
+    {
         _meshFilter.mesh = _towerConfig.TowerLevels[_index - 1].BuildingMesh;
+        yield return new WaitForSeconds(_delay);
+        _meshFilter.mesh = _towerConfig.TowerLevels[_index - 1].FinalMesh;
+        _meshSwitch = null;
     }
 }
