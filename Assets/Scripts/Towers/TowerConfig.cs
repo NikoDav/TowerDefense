802d123557e46554c0f781b1150b7904e15cadba
NikoDav <165057786+NikoDav@users.noreply.github.com>
2025-03-22 13:45:57 -0400

    create tower config

---
+++ Assets/Scripts/Towers/TowerConfig.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(fileName = "tower", menuName = "CreateTower")]
+public class TowerConfig : ScriptableObject
+{
+    [SerializeField] private List<TowerLevel> _towerLevels = new List<TowerLevel>();
+
+    public List<TowerLevel> TowerLevels { get => _towerLevels; set => _towerLevels = value; }
+}
+
+[Serializable]
+public class TowerLevel
+{
+    [SerializeField] private int _cost;
+    [SerializeField] private int _dmg;
+    [SerializeField] private double _range;
+    [SerializeField] private double _fireRate;
+    [SerializeField] private int _levelIndex;
+    [SerializeField] private Mesh _buildingMesh;
+    [SerializeField] private Mesh _finalMesh;
+
+    public int Cost { get => _cost; set => _cost = value; }
+    public int Dmg { get => _dmg; set => _dmg = value; }
+    public double Range { get => _range; set => _range = value; }
+    public double FireRate { get => _fireRate; set => _fireRate = value; }
+    public int LevelIndex { get => _levelIndex; set => _levelIndex = value; }
+    public Mesh BuildingMesh { get => _buildingMesh; set => _buildingMesh = value; }
+    public Mesh FinalMesh { get => _finalMesh; set => _finalMesh = value; }
+}
