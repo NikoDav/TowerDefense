74386c72240820bd173ac75535eb93ebadb1af54
NikoDav <165057786+NikoDav@users.noreply.github.com>
2025-04-20 12:24:49 -0400

    fix tower spawner

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -7,15 +7,32 @@ public class SpawnObject : MonoBehaviour
     [SerializeField] private Material _red;
     [SerializeField] private Material _green;
     [SerializeField] private MeshRenderer _currentObject;
+    [SerializeField] private float _radius;
+    [SerializeField] private LayerMask _collisionLayer;
     private Material _default;
+    private bool _canSpawn = false;
 
     private void Start()
     {
         _default = _currentObject.material;
     }
-
+    private void Update()
+    {
+        if(Physics.CheckSphere(transform.position, _radius, _collisionLayer))
+        {
+            _canSpawn = false;
+            _currentObject.material = _red;
+        }
+        else
+        {
+            _canSpawn = true;
+            _currentObject.material = _green;
+        }
+    }
     public void ChangeMaterial(bool canPlace)
     {
+        if (!_canSpawn)
+            return;
         if (canPlace)
         {
             _currentObject.material = _green;
